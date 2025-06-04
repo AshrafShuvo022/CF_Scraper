5b2c52bbfcfaf27f67cc0fae12c472489e32b944
Ashraf Uz Zaman <58702351+AshrafShuvo022@users.noreply.github.com>
2025-06-04 07:56:07 +0600

    Update main.py added frontend path

--- Backend_Scrap/main.py
+++ Backend_Scrap/main.py
@@ -11,7 +11,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+        "https://cf-scraper-beryl.vercel.app", ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
